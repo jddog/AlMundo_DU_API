17f6134db7a562eaaf64aa5bc43905a7a428550c
Daniel Diaz <teamdevelopmt@gmail.com>
2018-04-30 00:22:55 -0500

    Se agrega puerto local

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var hotelRout = require('./routs/hotelsRout');
 var bodyParser = require('body-parser');
 
-
 var app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, (error) => {
     console.log("Servidor corriendo :",PORT);
